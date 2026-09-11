1863dd6b9a9bf610d89c2b8a126aa2901f8a6641
Aaron Eisenberg <aaron@ironmthome.com>
2026-09-11 10:34:05 -0400

    Add cover letter page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const root = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
 	publicDir: 'static',
 	build: {
 		outDir: 'public',
+		rollupOptions: {
+			input: {
+				main: resolve(root, 'index.html'),
+				'supporting-cast': resolve(root, 'supporting-cast.html'),
+			},
+		},
 	},
 });
