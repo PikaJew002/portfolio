68ca6ea8a34b48278aa65b86714458a082e87a74
Aaron Eisenberg <aaron@ironmthome.com>
2026-09-11 16:31:46 -0400

    Add dynamic years experience, GitHub link, site link

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+let yearsExperience = document.querySelector('#years-experience');
+let currentDate = new Date();
+let startDate = new Date('2020-09-15');
+let yearsDiff = currentDate.getFullYear() - startDate.getFullYear();
+yearsExperience.textContent = yearsDiff;
